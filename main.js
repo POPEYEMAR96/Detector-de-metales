f22a9d824bc6a0172ff2d1b1d0e3bbfdca9cb686
POPEYEMAR96 <92955689+POPEYEMAR96@users.noreply.github.com>
2022-07-04 19:05:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+  }
+
+  function modelLoaded() {
+     console.log('El modelo ha sido cargado con exito');
+     }
+  
+     function draw() { 
+        image(video, 0, 0, 300, 300); 
+        classifier.classify(video, gotResult);
+     }
+  
+  
